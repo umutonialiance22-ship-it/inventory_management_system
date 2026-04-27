a64d0ad503a29d9969f80842bc0891006832f732
UMUTONI Aliance <umutonialiance22@gmail.com>
2026-04-27 13:11:58 +0200

    added new features/updated API

--- suppliers/serializers.py
+++ suppliers/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from .models import Supplier
-class SupplierSerializer(serializers.ModelSserializer):
+class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model=Supplier
-        field='__all__'
+        fields='__all__'
         
